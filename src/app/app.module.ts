5aafe42d13cfe23a2122682d76056ac52d5c1da9
Brendan Lestander <blestander@pitt.edu>
2020-06-06 17:40:48 -0400

    Generated and integrated skeleton MenuComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AuthComponent } from './auth/auth.component';
 import { RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         FooterComponent,
-        AuthComponent
+        AuthComponent,
+        MenuComponent
     ],
     imports: [
         BrowserModule,
