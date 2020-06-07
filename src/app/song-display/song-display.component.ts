333c51e7a81f95667feece0bb42dfa984bfb3303
Brendan Lestander <blestander@pitt.edu>
2020-06-07 07:58:46 -0400

    Songs passed to and (kind of) outputted by display

--- src/app/song-display/song-display.component.ts
+++ src/app/song-display/song-display.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PlaylistTrackObject } from '../playlist';
 
 @Component({
     selector: 'app-song-display',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./song-display.component.css']
 })
 export class SongDisplayComponent implements OnInit {
+
+    @Input() item: PlaylistTrackObject;
 
     constructor() { }
 
