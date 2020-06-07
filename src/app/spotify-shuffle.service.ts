08e52416bbcd41dd7dd3f804505c9893365587b0
Brendan Lestander <blestander@pitt.edu>
2020-06-07 10:34:34 -0400

    Implemented perfect collaborator mix.

--- src/app/spotify-shuffle.service.ts
+++ src/app/spotify-shuffle.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ShuffleRequest } from './shuffle-request';
 import { Observable, of } from 'rxjs';
 import { SpotifyService } from './spotify.service';
-import { toArray, map, mergeMap, exhaust } from 'rxjs/operators';
+import { toArray, map, mergeMap} from 'rxjs/operators';
 import { PlaylistTrackObject } from './playlist';
 
 @Injectable({
@@ -27,7 +27,7 @@ export class SpotifyShuffleService {
             case "true-shuffle":
                 return this.shuffleTrueShuffle(songs);
             case "perfect-mix":
-                return songs;
+                return this.shufflePerfectCollaboratorMix(songs);
             case "pass-through":
             default:
                 return songs;
@@ -43,4 +43,53 @@ export class SpotifyShuffleService {
         }
         return songs;
     }
+
+    private shufflePerfectCollaboratorMix(songs: PlaylistTrackObject[]) {
+        let results: PlaylistTrackObject[] = [];
+
+        // Make the buckets
+        let buckets: Buckets = this.bucketSongs(songs);
+
+        // While there are still buckets
+        while (Object.keys(buckets).length > 0) {
+            // Grab a song from each bucket
+            let round = [];
+            for (const user of Object.keys(buckets))
+                round.push(buckets[user].pop());
+
+            // Shuffle the round
+            round = this.shuffleTrueShuffle(round);
+
+            // Insert the shuffled round into results
+            results.splice(results.length, 0, ...round);
+
+            // Drop empty buckets
+            for (const user of Object.keys(buckets))
+                if (buckets[user].length == 0)
+                    delete buckets[user];
+        }
+
+        return results;
+    }
+
+    private bucketSongs(songs: PlaylistTrackObject[]): Buckets {
+        // Make buckets
+        let buckets = {};
+        for (const song of songs) {
+            let user_id = song.added_by.id;
+            if (buckets[user_id]) // Bucket exists
+                buckets[user_id].push(song);
+            else // Bucket needs to be made
+                buckets[user_id] = [ song ];
+        }
+
+        // Shuffle buckets
+        for (const id of Object.keys(buckets))
+            buckets[id] = this.shuffleTrueShuffle(buckets[id]);
+        return buckets;
+    }
+}
+
+interface Buckets {
+    [key: string]: PlaylistTrackObject[]
 }
