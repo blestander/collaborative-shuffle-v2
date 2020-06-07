f7498539eecf567982075453e2e41de1690fcf58
Brendan Lestander <blestander@pitt.edu>
2020-06-07 07:53:33 -0400

    Corrected indentation.

--- src/app/song-display/song-display.component.ts
+++ src/app/song-display/song-display.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-selector: 'app-song-display',
-templateUrl: './song-display.component.html',
-styleUrls: ['./song-display.component.css']
+    selector: 'app-song-display',
+    templateUrl: './song-display.component.html',
+    styleUrls: ['./song-display.component.css']
 })
 export class SongDisplayComponent implements OnInit {
 
-constructor() { }
+    constructor() { }
 
-ngOnInit(): void {
-}
+    ngOnInit(): void {
+    }
 
 }
