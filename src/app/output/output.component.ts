2203328d8be8f2762371a63aa676df031cc20ffc
Brendan Lestander <blestander@pitt.edu>
2020-06-07 08:46:52 -0400

    Moved song display logic into OutputComponent.

--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -15,4 +15,20 @@ export class OutputComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    getTitle(song: PlaylistTrackObject): string {
+        return song.track.name;
+    }
+
+    getArtists(song: PlaylistTrackObject): string[] {
+        return ["Some Artist"];
+    }
+
+    getUsername(song: PlaylistTrackObject): string {
+        return "Some user";
+    }
+
+    getImageSrc(song: PlaylistTrackObject): string {
+        return "https://dummyimage.com/300x300/000000/aaaaaa.png";
+    }
+
 }
